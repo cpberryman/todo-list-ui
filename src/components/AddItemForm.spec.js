302fe8394f683024613a3f1669ade8a0770c5059
cpberryman <c.p.berryman@btinternet.com>
2019-04-26 16:20:47 +0100

    added unit tests

---
+++ src/components/AddItemForm.spec.js
@@ -0,0 +1,45 @@
+/*global test*/
+
+import React from 'react'
+import AddItemForm from './AddItemForm.js'
+import jesttestsetup from '../../test/jestsetup.js'
+
+describe('AddItemForm', () => {
+
+  const wrapper = shallow(<AddItemForm/>)
+  const textBox = wrapper.find('.addToDoInput')
+  const button = wrapper.find('.addToDoButton')
+
+  it('renders', () => {
+    expect(wrapper.length).toBe(1)
+  })
+
+  it('renders the text box', () => {
+    expect(textBox.length).toBe(1)
+  })
+
+  it('renders the button', () => {
+    expect(button.length).toBe(1)
+  })
+
+  it('adds the to do item when button is pressed', () => {
+    const toDo = 'a to do item'
+
+    textBox.simulate('change', { target: { value: toDo } })
+
+    button.simulate('click')
+
+    expect(wrapper.find('p').text()).toBe(toDo)
+  })
+
+  it('foo', () => {
+    let p = wrapper.find('p')
+
+    p.simulate('click')
+
+    p = wrapper.find('p')
+
+    expect(p.length).toBe(0)
+  })
+
+});
