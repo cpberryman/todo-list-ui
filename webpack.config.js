dc208ec355c0d918948edf8a48910fb40f3f1130
CBerryman <guardscorpian@gmail.com>
2019-05-06 20:14:33 +0100

    added materials ui components for styling

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-	entry: './src/index.js',
+	entry: './src/app.js',
     output: {
       path: path.resolve('./src'),
       filename: "main.js",
